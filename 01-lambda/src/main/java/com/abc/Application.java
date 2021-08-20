18a34ccb99788be1a96002cb46e76bbd5fc9c0da
seerh <andykyo@126.com>
2021-08-20 20:33:59 +0800

    333

--- 01-lambda/src/main/java/com/abc/Application.java
+++ 01-lambda/src/main/java/com/abc/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	//
+	//11
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
